d8b14485b20f36b460cc19ea7eb10b5e312f4e53
Cocomong98 <yonghjjm0618@gamil.com>
2022-05-08 20:42:17 +0900

    .

---
+++ .github/ISSUE_TEMPLATE/a.c
@@ -0,0 +1,28 @@
+#define _CRT_SECURE_NO_WARNINGS    // strcpy 보안 경고로 인한 컴파일 에러 방지
+#include <stdio.h>
+#include <string.h>    // strcpy 함수가 선언된 헤더 파일
+
+typedef struct _Info {   // 구조체 정의
+    char name[20];        // 강아지 이름
+    int age;              // 강아지 나이
+    int weight;    // 강아지 몸무게
+    int birth; // 강아지 생일
+    int walktime; // 산책시간 
+    char foodcheck; // 다 먹었는지 여부 (Y,N)
+    char med[100]; // 정기검진 결과
+    char diary[200]; // 성장일기 
+} info;
+
+void visual () {
+    printf("========================================");
+}
+
+
+
+int main () {
+
+
+
+
+    return 0;
+}
